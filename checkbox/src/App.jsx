38550bfa72b7054d23e5e3fb6a052336dcae05c8
Phakaphol Dherachaisuphakij <64467457+GodzK@users.noreply.github.com>
2024-06-05 05:37:43 +0700

    finish

--- checkbox/src/App.jsx
+++ checkbox/src/App.jsx
@@ -2,9 +2,9 @@ import './App.css'
 import Card from "./components/Card"
 function App() {
   return (
-    <>
+    <div >
      <Card/>
-    </>
+    </div>
   )
 }
 
--- checkbox/src/components/Card.jsx
+++ checkbox/src/components/Card.jsx
@@ -4,31 +4,45 @@ function Card() {
   return (
     <div className='container'>
       <div className='card'>
-          <h1>Contract Us</h1>
+          <h2>Contract Us</h2>
           <div className='f-row-input'>
-          <h2>Firstname</h2>
+            <div className='box'>
+               <h5>Firstname</h5>
           <input type="text" />
-          <h2>Last Name</h2>
-          <input type="text" />
-          </div>
-          <div>
-            <h2>Email Address</h2>
-            <input type="text" />
-          </div>
-          <div>
-            <h2>Query Type</h2>
-            <div className='f-row-input'>
-                <input type="text" /> <input type="text" />
             </div>
-            <div>
-              <h2>Message</h2>
+          <div className='box'>
+             <h5>Last Name</h5>
+          <input type="text" />
+          </div>
+         
+          </div>
+          <div className='box'>
+            <h5>Email Address</h5>
+            <input type="text" className='input' />
+          </div>
+          <div className='box'>
+            <h5>Query Type</h5>
+            <div className='f-row-input'>
+              <div className='box-office'>
+                <label >
+                  <input type="checkbox" />General Enquiry
+                </label>
+              </div>
+              <div className='box-office'>
+                <label >
+                  <input type="checkbox" />Support Request
+                </label>
+              </div>
+            </div>
+            <div className='box'>
+              <h5>Message</h5>
               <textarea name="" id=""></textarea>
             </div>
             <div className='f-row-input'> 
               <input type="checkbox" />
             <p>I consent to being contacted by the team</p></div>
-           <div>
-            <button>Submit</button>
+           <div className='box'>
+            <button className='btn'>Submit</button>
            </div>
           </div>
        
